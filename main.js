8eec9afd1b80424a043c3618f0997b576d4928b8
Michael Harris <mwharris333@gmail.com>
2015-02-17 14:09:06 -0500

    create class for putting tweet content into ,
    some code cleanup

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-	var $body = $('body');
-	$body.html('');
+	var body = $('body');
+	var tweetBox = $('.tweetBox');
 
-	var index = streams.home.length - 1;
-	while(index >= 0){
-		var tweet = streams.home[index];
-		var $tweet = $('<div></div>');
-		$tweet.text('@' + tweet.user + ': ' + tweet.message);
-		$tweet.appendTo($body);
-		index -= 1;
+	for (var i=0; i<streams.home.length; i++){
+
+		var tweet = streams.home[i];
+		var tag = $('<div></div>');
+		tag.text('@' + tweet.user + ': ' + tweet.message);
+		tag.appendTo(tweetBox);
+
 	}
 
 });
