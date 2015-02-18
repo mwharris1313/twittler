564c13a63b9ad5430aca095774caf178ec6541f2
Michael Harris <mwharris333@gmail.com>
2015-02-18 15:19:09 -0500

    filter tweets from specific user ,
    fix error when len is smaller than maxTweets.

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-var r = 0;
+var _;
+var log = console.log.bind(console);
+
+streams.users['billy'] = [];
+
 $(document).ready(function(){
 
-	streams.users['billy'] = [];
+	var msg = ['j0','a1','b2','c3','d4','e5','f6','g7','h8','i9'];
+	var billyTweet = function() { makeTweet( 'billy', msg[Math.floor(Math.random()*10)] ); };
+	setInterval(billyTweet, 2000);
 
-	var testArg = "helloWorld";
-	var amount = 10;
-	checkTweets(amount);
-	setInterval(checkTweets, 1000, amount);
+	setInterval(checkTweets, 1000, 10, 'billy');
+	//setInterval(checkTweets, 1000, 20);
 
 });
 
-var checkTweets = function(amount){
-	r++;
-	var msg = ['j0','a1','b2','c3','d4','e5','f6','g7','h8','i9'];
-	makeTweet('billy', 'talked to ' + msg[r%10]);
+var checkTweets = function(amount, user){
 
 	var body = $('body');
 	var tweetBox = $('.tweetBox');
 	tweetBox.html('');
 
-	var len = streams.home.length;
-	for (var i = len-1; i >= (len-amount); i--){
+	var tweets = !user ? streams.home :
+		_.filter(streams.home, function(item){ return item.user === user; });
 
-		var tweet = streams.home[i];
+	var len = tweets.length;
+	var maxTweets = len < amount ? len : amount;
+
+	for (var i = len-1; i >= (len-maxTweets); i--){
+
+		var tweet = tweets[i];
 		var tag = $('<div></div>');
 		tag.text(tweet.created_at +  ' @' + tweet.user + ': ' + tweet.message);
 		tag.appendTo(tweetBox);
