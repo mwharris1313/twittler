bd9ce168c1d3639c8f1bfb1ae3ad1454ed8abe58
Michael Harris <mwharris333@gmail.com>
2015-02-19 18:32:52 -0500

    pause message stream with click event on 'PAUSE' text

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var _;
 var log = console.log.bind(console);
 streams.users['billy'] = [];
+var g = {};
+g.isPaused = false;
 
 $(document).ready(function(){
 	var msg = ['j0','a1','b2','c3','d4','e5','f6','g7','h8','i9'];
@@ -8,49 +10,58 @@ $(document).ready(function(){
 	setInterval(billyTweet, 2000);
 
 	//setInterval(checkTweets, 1000, {amount:10, user:'billy', isTimeFriendly:true});
+	$('.headRight').on('click', function(){
+		g.isPaused = !g.isPaused;
+	});
+
 	setInterval(checkTweets, 500, {amount:0, user:'billy', isTimeFriendly:false});
+
 
 });
 
 var checkTweets = function(args){
-	var amount 			= args.amount;
-	var user			= args.users;
-	var isTimeFriendly	= args.isTimeFriendly;
+	if (!g.isPaused){	
 
-	var body = $('body');
-	var segmentWrapper = $('.segmentWrapper');
-	segmentWrapper.html('');
+		var amount 			= args.amount;
+		var user			= args.users;
+		var isTimeFriendly	= args.isTimeFriendly;
 
-	var tweets = !user ? streams.home :
-		_.filter(streams.home, function(item){ return item.user === user; });
+		var body = $('body');
+		var segmentWrapper = $('.segmentWrapper');
+		segmentWrapper.html('');
 
-	var len = tweets.length;
-	var maxTweets = len < amount ? len : amount;
+		var tweets = !user ? streams.home :
+			_.filter(streams.home, function(item){ return item.user === user; });
 
-	if (amount !== 0){		
-		for (var i = len-1; i >= (len-maxTweets); i--){
+		var len = tweets.length;
+		var maxTweets = len < amount ? len : amount;
 
-			var tweet = tweets[i];
-			var createdAt = isTimeFriendly ? dateToFriendly(tweet.created_at) : 
-				dateToFull(tweet.created_at);
+		if (amount !== 0){		
+			for (var i = len-1; i >= (len-maxTweets); i--){
 
-			var tag = $('<div class="segment"></div>');
-			tag.text(createdAt +  ' @' + tweet.user + ': ' + tweet.message);
-			tag.appendTo(segmentWrapper);
-		}
-	} else {
-		for (var i = len-1; i >= 0; i--){
+				var tweet = tweets[i];
+				var createdAt = isTimeFriendly ? dateToFriendly(tweet.created_at) : 
+					dateToFull(tweet.created_at);
 
-			var tweet = tweets[i];
-			var createdAt = isTimeFriendly ? dateToFriendly(tweet.created_at) : 
-				dateToFull(tweet.created_at);
+				var tag = $('<div class="segment"></div>');
+				tag.text(createdAt +  ' @' + tweet.user + ': ' + tweet.message);
+				tag.appendTo(segmentWrapper);
+			}
+		} else {
+			for (var i = len-1; i >= 0; i--){
 
-			var tag = $('<div class="segment"></div>');
-			var left = '<span class="segLeft">'+createdAt+'</span>';
-			var middle = '<span class="segMiddle">'+' @' + tweet.user+': </span>';
-			var right = '<span class="segRight">'+tweet.message+'</span>';
-			tag.html(left+middle+right);
-			tag.appendTo(segmentWrapper);
+				var tweet = tweets[i];
+				var createdAt = isTimeFriendly ? dateToFriendly(tweet.created_at) : 
+					dateToFull(tweet.created_at);
+
+				var tag = $('<div class="segment"></div>');
+				var left = '<span class="segLeft">'+createdAt+'</span>';
+				var middle = '<span class="segMiddle">'+' @' + tweet.user+': </span>';
+				var right = '<span class="segRight">'+tweet.message+'</span>';
+				tag.html(left+middle+right);
+				tag.appendTo(segmentWrapper);
+			}
+
 		}
 
 	}
